c167a5bffcd904c09f957ce19485515bda0980eb
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-08-01 21:46:13 -0300

    Fiz a primeira textfield

---
+++ app/src/main/java/br/com/etecia/mymmock/AccountActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mymmock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/mymmock/StartActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mymmock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_layout);
+    }
+}
